e0d9c589c1f6d5160fb89525160f216ed5dcea91
wesome4311 <shortcm@clarkson.edu>
2020-09-28 21:54:39 -0400

    added in the majority of decrypt and encrypt functions, though lowercase seems to break them

--- src/data/ClackData.java
+++ src/data/ClackData.java
@@ -40,6 +40,7 @@ public abstract class ClackData implements Clack{
 		this(CONSTANT_LISTUSERS); //type currently defaults to list users
 	}
 
+	
 	//methods
 	/**
 	 * returns the type
@@ -74,12 +75,76 @@ public abstract class ClackData implements Clack{
 	 * abstract method, decrypts and returns either a message or a file's contents
 	 */
 	public abstract String getData(String key);
-
+	
+	public String lengthen(String line, String key) { 
+		int i = 0;
+	    while (true) 
+	    { 
+	        if (line.length() == i) 
+	            i = 0; 
+	        if (key.length() == line.length()) 
+	            break;
+	        key+=(key.charAt(i)); 
+	        i++;
+	    }
+	    //used https://www.geeksforgeeks.org/vigenere-cipher/ as a reference
+	    return key; 
+	}
+	
 	protected String encrypt( String inputStringToEncrypt, String key ){
-		return null; //@TODO
+		//need to lengthen the key to the length of the string
+		key = lengthen(inputStringToEncrypt, key);
+		
+		String encrypted="";
+		for (int i = 0; i < inputStringToEncrypt.length(); i++){
+			//following if statements test if differentiate if its uppercase, lowercase or something else(in which case it just gets the original)
+			if (Character.isUpperCase(inputStringToEncrypt.charAt(i))) {
+				int y = (inputStringToEncrypt.charAt(i) + key.charAt(i)) %26;
+				// convert into uppercase character
+		        y += 'A';
+		        encrypted += (char)(y);
+			}
+			else if (Character.isLowerCase(inputStringToEncrypt.charAt(i))) {
+				int y = (inputStringToEncrypt.charAt(i) + Character.toLowerCase(key.charAt(i))) %26;
+				// convert into lowercase character
+		        y += 'a';
+		        encrypted += (char)(y);
+			}
+			else {
+				//simply adding non-letter characters to the encrypted string, as we don't have to include them
+				encrypted += inputStringToEncrypt.charAt(i);
+			}
+	    }
+		//used https://www.geeksforgeeks.org/vigenere-cipher/ as a reference
+	    return encrypted;
 	}
 
 	protected String decrypt( String inputStringToDecrypt, String key ){
-		return null; //@TODO
+	    //need to lengthen the key to the length of the string
+		key = lengthen(inputStringToDecrypt, key);
+		
+		String decrypted=""; 
+	    for (int i = 0 ; i < inputStringToDecrypt.length(); i++) {
+	    	//following if statements test if differentiate if its uppercase, lowercase or something else(in which case it just gets the original)
+			if (Character.isUpperCase(inputStringToDecrypt.charAt(i))) {
+				int x = (inputStringToDecrypt.charAt(i) - key.charAt(i) + 26) %26;
+		        // convert into an uppercase character 
+		        x += 'A'; 
+		        decrypted += (char)(x); 
+			}
+			else if (Character.isLowerCase(inputStringToDecrypt.charAt(i))) {
+				int x = (inputStringToDecrypt.charAt(i) - Character.toLowerCase(key.charAt(i)) + 26) %26;
+				  
+		        // convert into lowercase character 
+		        x += 'a'; 
+		        decrypted += (char)(x); 
+			}
+			else {
+				//simply adding non-letter characters to the encrypted string, as we don't have to include them
+				decrypted += inputStringToDecrypt.charAt(i);
+			}
+	    }
+	  //used https://www.geeksforgeeks.org/vigenere-cipher/ as a reference
+	  return decrypted;
 	}
 }
