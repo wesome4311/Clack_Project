8b1792aa68c78a2e6ddd6d0674954af0692c8460
wesome4311 <shortcm@clarkson.edu>
2020-11-02 23:50:13 -0500

    Added in javadoc comments on ClientSideServerListener

--- src/main/ClientSideServerListener.java
+++ src/main/ClientSideServerListener.java
@@ -2,14 +2,28 @@ package main;
 
 import data.ClackData;
 
+
+/**
+ * @author MrCol
+ * ClientSideServerListener is a runnable class that works alongside ClackClient to print and receive data from the server
+ */
 public class ClientSideServerListener implements Runnable{ //reminder to myself to create javadoc for this once I'm done
 	
 	private ClackClient client;
 	
+	
+	/**
+	 * Constructor that takes in a ClackClient variable and sets it to the client variable
+	 * @param client
+	 */
 	public ClientSideServerListener(ClackClient client){
 		this.client = client;
 	}
-
+	
+	
+	/**
+	 * prints data that the client gets receives from the server
+	 */
 	@Override
 	public void run() {
 		while(!client.closeConnection) {
